9af8a92595af86841a8db334d3094e820f0e2457
acriles <115553419+acriles@users.noreply.github.com>
2023-11-09 12:14:12 -0300

    Update cardapio.h

--- cardapio.h
+++ cardapio.h
@@ -2,13 +2,16 @@
 #include <vector>
 #include <iostream>
 #include "pizza.h"
+#include "bebida.h"
 
 using namespace std;
 
 class Cardapio{
     private: 
-    vector <Pizza> pizzas;
+    vector <Pizza> pizzas; //vetor com o tipo pizza (sabor, tamanho..) em cada posicao 
+    vector <Bebida> bebidas; //vetor com o tipo bebida 
     public:
-    void inicializarCardapio();
-    vector <Pizza> getPizzas();
-}
+    void inicializarCardapio(); //o administrador consegue inicializar o cardapio conforme quiser 
+    void getPizzas(); //mostra as pizzas do cardapio
+    void getBebidas(); //mostra as bebidas do cardapio 
+};
