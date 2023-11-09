aed52f0c7f213b109904e9c032292065665b6679
acriles <115553419+acriles@users.noreply.github.com>
2023-11-09 12:13:46 -0300

    Update cardapio.cpp

--- cardapio.cpp
+++ cardapio.cpp
@@ -2,8 +2,31 @@
 #include "cardapio.h"
 
 void Cardapio::inicializarCardapio(){
+    //inicializando as pizzas do cardapio 
+    pizzas.push_back(Pizza("Calabresa", "Pequena", {}, 39));
+    pizzas.push_back(Pizza("Calabresa", "Média", {}, 49));
+    pizzas.push_back(Pizza("Calabresa", "Grande", {}, 59));
+    pizzas.push_back(Pizza("Marguerita", "Pequena", {}, 39));
+    pizzas.push_back(Pizza("Marguerita", "Média", {}, 49));
+    pizzas.push_back(Pizza("Marguerita", "Grande", {}, 59));
+
+    //inicializando as bebidas do cardapio 
+    bebidas.push_back(Bebida("Refrigerante", "Coca", "Lata", 5.00));
+
+    
+    
 }
 
-vector <Pizza> Cardapio::getPizzas(){
-    return  pizzas;
+void Cardapio::getPizzas(){
+     cout << "Pizzas"; 
+    for (int i=0; i<pizzas.size(); i++){
+        cout << pizzas[i] << endl;
+    }
+    cout << endl;
+}
+
+void Cardapio::getBebidas(){
+    cout <<"Bebidas";
+
+
 }
