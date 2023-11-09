daf729ea29037f0d7b174a26ae5b740e4391adad
acriles <115553419+acriles@users.noreply.github.com>
2023-11-09 12:18:55 -0300

    Update bebida.hpp

--- bebida.hpp
+++ bebida.hpp
@@ -5,19 +5,22 @@ using namespace std;
 class Bebida {
 private:
   string sabor;
+  string tipo;
   string tamanho;
   float valor;
 
 public:
-  Bebida(); // construtor
+  Bebida(string _tipo, string _sabor, string _tamanho, float _valor);
+  void setTipo(string _tipo);
   void setSabor(string _sabor);
   void setTamanho(string _tamanho);
   void setValor(float _valor);
 
   string getSabor();
+  string getTipo();
   string getTamanho();
   float getValor();
-  void imprime_Bebida();
+  void imprimeBebida();
 
   ~Bebida(void); // destrutor
 };
