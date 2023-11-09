efb1a345195b982f4e544ab44d1beb9f07faef19
acriles <115553419+acriles@users.noreply.github.com>
2023-11-09 12:17:44 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -37,6 +37,17 @@ using namespace std;
     pizza1.addAdicional(adicional);
     pizza1.imprimePizza();
 
+    Bebida bebida1;
+   string sabor, tamanho;
+   cout << "Digite o sabor" << endl;
+   cin >> sabor;
+   bebida1.setSabor(sabor);
+   cout << "Digite o tamanho" << endl;
+   cin >> tamanho;
+   bebida1.setTamanho(tamanho);
+   bebida1.imprime_Bebida();
+  
+
 
     
     return 0;
